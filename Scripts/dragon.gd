0b001bedd03aef97e508007ad3d09b93d5334863
ArceusMaxis <octohands@gmail.com>
2024-08-14 19:28:53 +0530

    pinnacle dragon etc

--- Scripts/dragon.gd
+++ Scripts/dragon.gd
@@ -1,14 +1,22 @@
 extends CharacterBody3D
+
 @export var total_hp = 20
 @export var crystals_destroyed = 0
 @export var hover_speed = 2
 @export var cooldown_reduction = 0.0
-@export var teeth_scene :PackedScene
+@export var teeth_scene : PackedScene
+@export var patrol_radius = 10
+@export var patrol_center = Vector3(0, 6, -13)
+@export var patrol_speed = 0.5 
+
 var current_state = "patrol"
 var can_damage = false
 var hp = 20
+var player = null
+var patrol_angle = 0.0 
 
 func _ready():
+	position = patrol_center + Vector3(0, 0, -patrol_radius) 
 	set_physics_process(false)
 	patrol()
 
@@ -29,22 +37,27 @@ func patrol():
 	patrol_movement(0)
 
 func patrol_movement(delta):
-	# Circular patrol logic
-	rotate_y(hover_speed * delta)
-	# Play patrol sound effect, etc.
+	patrol_angle += patrol_speed * delta 
+	var x = patrol_center.x + patrol_radius * sin(patrol_angle)
+	var z = patrol_center.z + patrol_radius * cos(patrol_angle)
+	position = Vector3(x, patrol_center.y, z)
+	if int(patrol_angle) % int(TAU) == 0:
+		%Hurtsfx.play()
 
 func roar():
-	# Trigger roar sound and effect, stun player
-	pass
+	%Roar.play()
+	player = get_tree().get_first_node_in_group("player")
+	if player:
+		player.set_process(false)
+		player.set_physics_process(false)
+		await get_tree().create_timer(2)
 
 func full_field_attack():
 	current_state = "full_field"
-	# Rotate fast and spawn nails
 	pass
 
 func focus_attack():
 	current_state = "focus"
-	# Stop movement and fire nails beam at player
 	pass
 
 func nudge_player():
@@ -81,7 +94,7 @@ func drop_teeth():
 	pass
 
 func die():
-	queue_free()
+	velocity.y -= 1
 
 func minus_hp(dmg):
 	if can_damage:
@@ -90,7 +103,7 @@ func minus_hp(dmg):
 		else:
 			drop_teeth()
 			var dmg_tween = get_tree().create_tween()
-			dmg_tween.tween_property(%Anim, "modulate", Color(0, 1, 0, 1), 0.2)
+			dmg_tween.tween_property(%Anim, "modulate", Color(1, 0, 0, 1), 0.2)
 			dmg_tween.tween_property(%Anim, "modulate", Color(1, 1, 1, 1), 0.2)
 			%Hurtsfx.play()
 			hp -= dmg
